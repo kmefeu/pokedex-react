9921ef388f14788a2e22dd6a17e9317bbd343436
Matheus Corrêa <mcorreadesign@gmail.com>
2021-07-26 16:10:19 -0300

    style: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Home from "./pages/home";
+import "../src/styles/reset.css";
+import "../src/styles/font.css";
+import "../src/styles/global.css";
 
 function App() {
   return <Home />;
