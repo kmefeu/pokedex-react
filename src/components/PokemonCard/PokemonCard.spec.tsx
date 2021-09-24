3745b1f56d6c49cbd096c8366747a1589ac31b84
Matheus Corrêa <mcorreadesign@gmail.com>
2021-09-24 13:00:45 -0300

    feat: jest test config

---
+++ src/components/PokemonCard/PokemonCard.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from "@testing-library/react";
+import { Router } from "react-router";
+import { createMemoryHistory } from "history";
+import PokemonCard from "./index";
+
+// teste para ver se o componente esta renderizando da forma correta
+test("PokemonCard renders correctly", () => {
+  // cria um history mockado para conseguir renderizar o componente que tem essa dependencia
+  const history = createMemoryHistory();
+  // render faz o render virtual do componente
+  const { debug } = render(
+    <Router history={history}>
+      <PokemonCard
+        loading={false}
+        LoadingIndex={1}
+        key="1"
+        name="Bulbasaur"
+        id="1"
+        types={[
+          { pokemon_v2_type: { name: "grass" } },
+          { pokemon_v2_type: { name: "poison" } },
+        ]}
+        sprites={
+          "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/1.gif"
+        }
+      />
+    </Router>
+  );
+
+  // imprime no console a saida do componente que esta dentro do debug
+  debug();
+});
